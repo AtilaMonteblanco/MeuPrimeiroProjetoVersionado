a450682860eba9905cf7839509c4e0fda757cd30
Tecnico - TI <tecnico@epfserver.com.br>
2016-04-26 22:51:15 -0300

    Criação da classe calculadora e comportamentos

---
+++ src/Calculadora.java
@@ -0,0 +1,23 @@
+
+public class Calculadora {
+
+	private int valorUm;
+	private int valorDois;
+
+	public void somar() {
+
+	}
+
+	public void subtracao() {
+
+	}
+
+	public void dividir() {
+
+	}
+
+	public void multiplicar() {
+
+	}
+
+}
