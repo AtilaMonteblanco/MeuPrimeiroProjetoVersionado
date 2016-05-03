660783ba90f53e341f390e5ec94cbed32cd8b884
Tecnico - TI <tecnico@epfserver.com.br>
2016-05-02 22:43:09 -0300

    Inclusão Função Soma

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,11 +1,25 @@
 
 public class Calculadora {
 
-	private int valorUm;
-	private int valorDois;
+	// private int ValorUm; //private porque só interessa para esta classe.
+	// private int ValorDois;//private porque só interessa para esta classe.
+	private int valor; // Acumulador para quardar valores anteriores para
+						// futuras operações.
+	// public(Metodo) porque vou usar estas informações na classe "Principal"
 
-	public void somar() {
-
+	public void somar(int ValorUm, int ValorDois) { // void é um metodo que não
+													// retorna nada
+		/*
+		 * this.ValorUm = ValorUm; // "this" valo de escopo da classe.
+		 * this.ValorDois = ValorDois; //No modo Debug clicando "F6" executa o
+		 * cmando de atualizar this.total += this.ValorUm + this.ValorDois; //+=
+		 * a esquerda recebe a direita + ele mesmo.
+		 */
+		// int soma = this.ValorUm + this.ValorDois; //this desta
+		// classe"Calculadora" e não do método.
+		// return soma; //por isso mudei de void para int na linha 7 para ter
+		// "return" na classe "Principal".
+		this.valor = this.valor + (ValorUm + ValorDois);
 	}
 
 	public void subtracao() {
@@ -18,6 +32,10 @@ public class Calculadora {
 
 	public void multiplicar() {
 
+	}
+
+	public int getValor() {
+		return valor;
 	}
 
 }
---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+	public static void main(String[] args){
+		//criando calculadora "Calculadora" é o tipo poderia ser String calculadora.
+
+		Calculadora calculadoraDoPedrinho = new Calculadora(); //"Instancia do objeto Calculadora
+		//começo do construtor(método que tem o mesmo nome da classe) " new esta invocando o constrotur
+
+
+		//calculadoraDoPedrinho.somar(20,60); //esta calculador esta invocando a propriedade soma da "Calculadora"
+		//int retornoCalculo = calculadoraDoPedrinho.somar(10,50);
+		//int retornoCalculoSegundaVez = calculadoraDoPedrinho.somar(20,10);
+		calculadoraDoPedrinho.somar(10,50);
+		calculadoraDoPedrinho.somar(20,10);
+		JOptionPane.showMessageDialog(null, calculadoraDoPedrinho.getValor()); // mostra painel.com resultado da soma "0"
+
+	}
+}
